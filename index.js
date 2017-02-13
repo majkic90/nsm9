b67b90d253d9a6b98e1df98da3da63f2d8280352
Aleksandar Majkic <aleksandar@comdata.rs>
2017-02-13 13:37:17 +0100

    First commit

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var ifERROR = false;
 var refreshTime = 12000;
 var refOneItem = 4;
 var knifes = [];
-var startTime = 9;
+var startTime = 0;
 
 io.on('connection', function (socket) {
     socket.send("connect");
