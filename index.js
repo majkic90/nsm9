f6380a29156bbf2610a7044bc5d8237b2e4865f1
majkic90 <majkic1990@gmail.com>
2017-02-13 16:04:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var ifERROR = false;
 var refreshTime = 12000;
 var refOneItem = 4;
 var knifes = [];
-var startTime = 0;
+var startTime = 8;
 
 io.on('connection', function (socket) {
     socket.send("connect");
